226afd7dd4756918a87bc59771bb04470dca00a7
Rohith <krohith1998@gmail.com>
2017-08-20 18:22:13 +0530

    Database Created

--- forum_DBSetup.py
+++ forum_DBSetup.py
@@ -1,6 +1,7 @@
+import datetime
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String,
+from sqlalchemy import Column, ForeignKey, Integer, String,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -12,28 +13,45 @@ class Users(Base):
 
     id = Column(Integer,primary_key=True)
     name = Column(String(25))
-    subscription = Column(String[30])
+    subscription = Column(String(30))
     privilage = Column(String)
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer,primary_key=True)
+    content = Column(String(1000))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    likes = Column(Integer)
+    users = relationship(Users)
     
 class Comment(Base):
     __tablename__='comment'
 
     id = Column(Integer,primary_key=True)
-    content = Column(String[250])
-    #date = Column(
+    content = Column(String(250))
+    created_date = Column(DateTime, default = datetime.datetime.utcnow)
     user_id = Column(Integer, ForeignKey('users.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
     likes = Column(Integer)
     users = relationship(Users)
-    post  = relationship(post)
-class Post(Base):
-    __tablename__ = 'post'
+    post  = relationship(Post)
+class ClubPost(Base):
+    __tablename__='clubpost'
 
     id = Column(Integer,primary_key=True)
-    content = Column(String[1000])
+    content = Column(String(1000))
     user_id = Column(Integer, ForeignKey('users.id'))
     likes = Column(Integer)
-class clubs(Base):
+    users = relationship(Users)
+    
+class Clubs(Base):
+    __tablename__ = 'clubs'
     id = Column(Integer,primary_key=True)
-    name = Column(String[50])
+    name = Column(String(50))
+    subscription_id = Column(String,ForeignKey('users.subscription'))
     user_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users)
+    
+	
+engine = create_engine('sqlite:///forum.db')
+Base.metadata.create_all(engine)
