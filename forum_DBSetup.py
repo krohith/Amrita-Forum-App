7190d2b45102d1ec19c145fd5bdfd6212655ea1b
Rohith <krohith1998@gmail.com>
2017-09-11 13:39:20 +0530

    Subscription

--- forum_DBSetup.py
+++ forum_DBSetup.py
@@ -1,57 +1,70 @@
 import datetime
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String,DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class Users(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(25))
-    subscription = Column(String(30))
-    privilage = Column(String)
+    rollno = Column(String(16))
+
+
+class Clubs(Base):
+    __tablename__ = 'clubs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+
+class Subscription(Base):
+    __tablename__ = 'subscription'
+    id = Column(Integer, primary_key=True)
+    club_id = Column(Integer, ForeignKey('clubs.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    value = Column(Integer)
+    users = relationship(Users)
+    clubs = relationship(Clubs)
+
+
 class Post(Base):
     __tablename__ = 'post'
 
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     content = Column(String(1000))
     user_id = Column(Integer, ForeignKey('users.id'))
     likes = Column(Integer)
     users = relationship(Users)
-    
-class Comment(Base):
-    __tablename__='comment'
 
-    id = Column(Integer,primary_key=True)
-    content = Column(String(250))
-    created_date = Column(DateTime, default = datetime.datetime.utcnow)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250), nullable="false")
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
     user_id = Column(Integer, ForeignKey('users.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
     likes = Column(Integer)
     users = relationship(Users)
-    post  = relationship(Post)
-class ClubPost(Base):
-    __tablename__='clubpost'
+    post = relationship(Post)
 
-    id = Column(Integer,primary_key=True)
+
+class ClubPost(Base):
+    __tablename__ = 'clubpost'
+
+    id = Column(Integer, primary_key=True)
     content = Column(String(1000))
     user_id = Column(Integer, ForeignKey('users.id'))
     likes = Column(Integer)
     users = relationship(Users)
-    
-class Clubs(Base):
-    __tablename__ = 'clubs'
-    id = Column(Integer,primary_key=True)
-    name = Column(String(50))
-    subscription_id = Column(String,ForeignKey('users.subscription'))
-    user_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship(Users)
-    
-	
+    club_id = Column(Integer,ForeignKey('clubs.id'))
+
 engine = create_engine('sqlite:///forum.db')
 Base.metadata.create_all(engine)
---
+++ test_populator.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from forum_DBSetup import Users, Base, Post , Comment , ClubPost , Clubs, Subscription
+
+engine = create_engine('sqlite:///forum.db')
+
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
