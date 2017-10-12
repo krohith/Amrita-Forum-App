2f73eb5b8ff76fb93176009b0578fb48c49761b2
Rohith <krohith1998@gmail.com>
2017-10-12 23:21:24 +0530

    Added some server code

--- Database/server.py
+++ Database/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from forum_DBSetup import Base, Users, Clubs, Subscription, Post, Comment, ClubPost, ClubComment
+import datetime
 
 app = Flask(__name__)
 
@@ -27,6 +28,31 @@ def display(ids):
 
     return jsonify(post=[k.serialize for p in post for k in p])
 
+
+@app.route('/create/<int:ids>', methods=['GET', 'POST'])
+def newPost(ids):
+    if request.method == 'POST':
+        data = request.json
+        newp = Post(content=data['content'], user_id=ids, likes=0,
+                    created_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        session.add(newp)
+        session.commit()
+    else:
+        pos = session.query(Post).filter_by(user_id=ids).all()
+        return jsonify(posts=[i.serialize for i in pos])
+
+
+@app.route('/<int:u_id>/<int:p_id>', methods=['GET', 'POST'])
+def like(u_id, p_id):
+    if request.method == 'POST':
+        post = session.query(Post).filter_by(user_id=u_id, id=p_id).one()
+        post.likes += 1
+        session.add(post)
+        session.commit()
+    else:
+        pos = session.query(Post).filter_by(user_id=u_id).all()
+        return jsonify(posts=[i.serialize for i in pos])
+
 if __name__ == '__main__':
     app.debug = True
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
