8d9a269ef12abd1ab79bfcc387bda2613cd4f954
Sanath2611 <kumarsanath2611@gmail.com>
2017-10-13 20:14:14 +0530

    Added Navigation Bar

---
+++ Android/AskAmrita/app/src/main/java/com/example/android/askamrita/activity_home.java
@@ -0,0 +1,32 @@
+package com.example.android.askamrita;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class activity_home extends AppCompatActivity {
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mDrawerlayout=(DrawerLayout) findViewById(R.id.sidebar);
+        mToggle=new ActionBarDrawerToggle(activity_home.this,mDrawerlayout,R.string.open,R.string.close);
+        mDrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
